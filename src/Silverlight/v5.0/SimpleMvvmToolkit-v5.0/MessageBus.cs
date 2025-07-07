23c10eda6ba06de9aa4df47b62c9a68ea9632a3a
Pierre Barjon <barjon.pierre@gmail.com>
2025-07-07 13:00:31 +0200

    Add sources

---
+++ src/Silverlight/v5.0/SimpleMvvmToolkit-v5.0/MessageBus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SimpleMvvmToolkit
+{
+    /// <summary>
+    /// Facilitates communication among view-models.
+    /// To prevent memory leaks weak references are used.
+    /// </summary>
+    public class MessageBus : MessageBusCore
+    {
+        private static MessageBus _instance;
+        private static readonly object StaticLock = new object();
+
+        // Prevent direct instantiation
+#if PORTABLE || XAMARIN
+        private MessageBus()
+            : base(null)
+        {
+        }
+#else
+        private MessageBus()
+            : base(UIDispatcher.Current)
+        {
+        }
+#endif
+
+        /// <summary>
+        /// Singleton of MessageBus.
+        /// </summary>
+        public static MessageBus Default
+        {
+            get
+            {
+                lock (StaticLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new MessageBus();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
